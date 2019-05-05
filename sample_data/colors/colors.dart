5cb8d2461e8d9f72b7c047c53caf67b67f0d118d
mtcliatt <matthewcliatt@gmail.com>
2019-05-04 18:25:59 -0700

    Add some colors.

---
+++ sample_data/colors/colors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+/// The same colors as Colors.primaries.
+static const List<MaterialColor> primaries = <MaterialColor>[
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.indigo,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.lightGreen,
+  Colors.lime,
+  Colors.yellow,
+  Colors.amber,
+  Colors.orange,
+  Colors.deepOrange,
+  Colors.brown,
+  Colors.blueGrey,
+];
+
+/// Pretty much Colors.primaries, but without a few of the darker ones.
+static const List<MaterialColor> lighterPrimaries = <MaterialColor>[
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.lightGreen,
+  Colors.lime,
+  Colors.yellow,
+  Colors.amber,
+  Colors.orange,
+  Colors.deepOrange,
+  Colors.blueGrey,
+];
